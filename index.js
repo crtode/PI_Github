f27cc182a224061f8f86ee76c9d95bf14d61618c
Cris <cristina.tornero.de.baro@gmail.com>
2025-12-11 15:48:53 +0100

    Add files via upload
    Cris ha añadido p004

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express(); //cargo express y crea nuestra app
+
+app.set("port", process.env.PORT || 8080); //para usar el puerto que le digo
+
+app.use(require("./routes/indexRoutes")); //conecto con las rutas que le he dado en el otro archivo y las usamos en la app
+
+// enciendo el servidor
+app.listen(app.get("port"), () => {
+  console.log("hola desde el puerto " + app.get("port")); //muestro por pantalla que el puerto esta funcionando con un hola
+});
